b987239977247b51128c6c9ccb2a02384d68387b
Nadia-2025 <nadyah-88@hotmail.com>
2025-08-13 13:14:38 +0200

    create footer component, adjust footer & cards css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 import Cards from "../data/Cardsdata";
+import "./Card.css";
 
 const Card = () => {
   return (
     <div className="container">
       <div className="row">
+        <h1 className="mb-5">Latest Recipes</h1>
         {Cards.map(({ image, title, description }, index) => (
           <div className="col-md-3" key={index}>
-            <div className="card" style={{ width: "100%" }}>
+            <div className="card mb-5" style={{ width: "100%" }}>
               <img src={image} className="card-img-top" alt="sandwich" />
-              <div className="card-body">
+              <div className="card-body text-start">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="#" className="btn btn-primary">
-                  Find out more!
+                <p className="card-text texto-corto">{description}</p>
+                <a href="#" className="btn btn-success">
+                  Read more!
                 </a>
               </div>
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,114 @@
+import "./Footer.css";
+
 const Footer = () => {
   return (
     <>
-      <h1></h1>
+      <footer className="mt-5">
+        <div className="container pt-5">
+          <div className="row">
+            <div className="col-3 text-start">
+              <span className="fs-3 fw-bold">
+                <i>Bread & Beyond</i>
+              </span>
+            </div>
+            <div className="col-3 fs-6 text-start">
+              <h4>Navigate</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Recipies
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Chefs
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Restaurants
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Ingredients
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className="col-3 fs-6 text-start">
+              <h4>Information</h4>
+              <ul className="list-unstyled">
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Work with us
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Insights & consultancy
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    FAQs
+                  </a>
+                </li>
+                <li className="text-decoration-none">
+                  <a className="text-decoration-none" href="#">
+                    About us
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Jobs
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a className="text-decoration-none" href="#">
+                    Terms and Conditions
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-3 fs-6 text-start">
+              <h4>Follow Us</h4>
+              <div className="media-section d-flex justify-content-start">
+                <i className="fa-brands fa-facebook mt-3 me-2 fs-3"></i>
+                <i className="fa-brands fa-instagram mt-3 me-2 fs-3"></i>
+                <i className="fa-brands fa-youtube mt-3 me-2 fs-3"></i>
+                <i className="fa-brands fa-x-twitter mt-3 me-2 fs-3"></i>
+                <i className="fa-brands fa-linkedin mt-3 me-2 fs-3"></i>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12 text-center fs-6">
+              <hr />
+              <p>
+                Copyright <i className="fa-regular fa-copyright"></i> Bread &
+                Beyond 2025
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
